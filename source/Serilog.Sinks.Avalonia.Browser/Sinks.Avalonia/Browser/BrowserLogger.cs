ff11fc28ef5fbae381ff36d8a1dfb6ba08c0c791
Tsaritsin S.S. <tsaritsin@biruza.com>
2024-10-16 12:34:43 +0300

    Fixed interop

--- source/Serilog.Sinks.Avalonia.Browser/Sinks.Avalonia/Browser/BrowserLogger.cs
+++ source/Serilog.Sinks.Avalonia.Browser/Sinks.Avalonia/Browser/BrowserLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices.JavaScript;
 using System.Runtime.Versioning;
+using System.Text.Json;
 
 namespace Serilog.Sinks.Avalonia.Browser;
 
@@ -7,42 +8,52 @@ namespace Serilog.Sinks.Avalonia.Browser;
 internal partial class BrowserLogger
 {
     [JSImport("globalThis.console.log")]
-    internal static partial Task Log([JSMarshalAs<JSType.Array<JSType.Object>>] object[] args);
+    internal static partial Task Log([JSMarshalAs<JSType.String>] string message);
 
     public void Write(object[] args)
     {
-        Log(args);
+        var message = JsonSerializer.Serialize(args);
+
+        Log(message);
     }
 
     [JSImport("globalThis.console.error")]
-    internal static partial Task LogError([JSMarshalAs<JSType.Array<JSType.Object>>] object[] args);
+    internal static partial Task LogError([JSMarshalAs<JSType.String>] string message);
 
     public void Error(object[] args)
     {
-        LogError(args);
+        var message = JsonSerializer.Serialize(args);
+
+        LogError(message);
     }
 
     [JSImport("globalThis.console.warn")]
-    internal static partial Task LogWarning([JSMarshalAs<JSType.Array<JSType.Object>>] object[] args);
+    internal static partial Task LogWarning([JSMarshalAs<JSType.String>] string message);
 
     public void Warning(object[] args)
     {
-        LogWarning(args);
+        var message = JsonSerializer.Serialize(args);
+
+        LogWarning(message);
     }
 
     [JSImport("globalThis.console.debug")]
-    internal static partial void LogDebug([JSMarshalAs<JSType.Array<JSType.Object>>] object[] args);
+    internal static partial void LogDebug([JSMarshalAs<JSType.String>] string message);
 
     public void Debug(object[] args)
     {
-        LogDebug(args);
+        var message = JsonSerializer.Serialize(args);
+
+        LogDebug(message);
     }
 
     [JSImport("globalThis.console.info")]
-    internal static partial Task LogInformation([JSMarshalAs<JSType.Array<JSType.Object>>] object[] args);
+    internal static partial Task LogInformation([JSMarshalAs<JSType.String>] string message);
 
     public void Information(object[] args)
     {
-        LogInformation(args);
+        var message = JsonSerializer.Serialize(args);
+
+        LogInformation(message);
     }
 }
